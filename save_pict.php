e80842a1ac13c8ffa09955e4870347d9b2c0e5dd
Satria Salim <77188249+SalimQS@users.noreply.github.com>
2023-05-19 13:30:13 +0800

    Delete save_pict.php

--- save_pict.php
+++
@@ -1,9 +0,0 @@
-<?php
-$photo = $_POST['photo'];
-$url = $_POST['photoName'];
-
-$photo = str_replace('data:image/png;base64,', '', $photo);
-$photo = str_replace(' ', '+', $photo);
-
-file_put_contents("picture/image/" . $url, $photo);
-?>
